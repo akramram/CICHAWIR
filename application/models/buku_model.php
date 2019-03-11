2a42b50b796e0849f5670caa5f125e602b7b0615
yohanesam <yohanesam@users.noreply.github.com>
2019-03-11 09:59:35 +0700

    add mvc book (ver.2)
    Co-Authored-By: yohanesam <yohanesam@users.noreply.github.com>

--- application/models/buku_model.php
+++ application/models/buku_model.php
@@ -1,15 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Buku extends CI_Model {
+class buku_model extends CI_Model {
 
     public function getBook() {
         return array(
             array(
-              'judul' => 'Belajar framework ci'
+              'judul' => 'Belajar framework ci',
+              'pengarang' => 'Budi Raharjo',
+              'penerbit' => 'Informatika'
             ),
             array(
-              'judul' => 'Belajar potosop'
+              'judul' => 'Belajar photoshop',
+              'pengarang' => 'Abdul Khadir',
+              'penerbit' => 'Andi Offset'
             )
         );
     }
--- application/views/buku_view.php
+++ application/views/buku_view.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
 <html>
-
-<head><title> Buku </title></head>
-
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
 <body>
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>Judul</th>
+            <th>Pengarang</th>
+            <th>Penerbit</th>
+        </tr>
+            
+        <?php
+            $no = 1;
+            for($i = 0; $i < count($data_buku); $i++) {
+                echo '<tr>';
+                    echo '<td>'.$no.'</td>';
+                    echo '<td>'.$data_buku[$i]['judul'].'</td>';
+                    echo '<td>'.$data_buku[$i]['pengarang'].'</td>';
+                    echo '<td>'.$data_buku[$i]['penerbit'].'</td>';
+                echo '</tr>';
 
-<table border="1">
-    <tr>
-    <th> No </th>
-    <th> Judul </th>
-    <th> Pengarang </th>
-    <th> Penerbit </th>
-    </tr>
-    <?php
-    $no = 1;
-    for($i=0;$i < count($data_buku); $i++) {
-        echo '<tr>';
-        echo '<td>' .$no. '</td>';
-        echo '<td>' .$data_buku[$i]['judul']. '</td>';
-        echo '<td>' .$data_buku[$i]['pengarang']. '</td>';
-        echo '<td>' .$data_buku[$i]['[penerbit]']. '</td>';
-    echo '</tr>';
 
-    $no++;
-    }
-?>
-</table>
-
+                $no++;
+            }
+        ?>
+    </table>
 </body>
-
 </html>
